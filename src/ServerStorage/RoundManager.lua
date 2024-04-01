efa431999a2823ed28089950f397285ed85f25f1
J3(James) <jpcourtney1220@gmaIl.com>
2024-04-01 08:37:06 -0400

    HealthPickups

--- src/ServerStorage/RoundManager.lua
+++ src/ServerStorage/RoundManager.lua
@@ -1,7 +1,11 @@
 local CollectionService = game:GetService("CollectionService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local Assets = ReplicatedStorage:WaitForChild("Assets")
+local MapAssets = Assets:WaitForChild("Map")
+
 local Events = require(ReplicatedStorage:WaitForChild("RepFiles"):WaitForChild("Events"))
+local HealthManager = require(ReplicatedStorage:WaitForChild("RepFiles"):WaitForChild("Combat"):WaitForChild("HealthManager"))
 
 local RoundManager = {}
 RoundManager.__index = RoundManager
@@ -16,7 +20,7 @@ end
 function RoundManager:Init(ServerGameManager)
     self.serverGameManager = ServerGameManager
 
-    self.belowLimit = 1
+    self.belowLimit = 0 --1
     self.maxTick = 1
     self.currTick = 0
 
@@ -34,6 +38,38 @@ function RoundManager:Init(ServerGameManager)
 
     --map selection will be needed later on
     self.Map = workspace.Map
+
+    self.healthPads = {}
+end
+
+function RoundManager:ConfigurePickups(pickupFolder)
+    for _, pad in pairs(pickupFolder:GetChildren()) do
+        local pickup = MapAssets.Pickups.HealthPickup:Clone()
+        pickup.PrimaryPart.Position = pad.Position
+        pickup.Parent = workspace.Ignore
+
+        self.healthPads[pad] = {}
+        self.healthPads[pad].pad = pickup
+        self.healthPads[pad].isActive = true
+        self.healthPads[pad].hitbox = pickup.Hitbox
+        self.healthPads[pad].Cube = pickup.Cube
+        self.healthPads[pad].cooldown = 10
+        self.healthPads[pad].currTime = 0
+        self.healthPads[pad].UI = pickup.PrimaryPart.Cooldown
+        self.healthPads[pad].Timer = pickup.PrimaryPart.Cooldown.Timer
+
+        pad.Transparency = 1
+    end
+end
+
+function RoundManager:CleanupPickups()
+    for padId, padData in pairs(self.healthPads) do
+        if padData.pad then
+            padData.pad:Destroy()
+        end
+
+        self.healthPads[padId] = nil
+    end
 end
 
 function RoundManager:TeleportAllPlayers()
@@ -100,8 +136,75 @@ function RoundManager:TeleportPlayer(player: Player)
     character:PivotTo(teleporter.CFrame * CFrame.new(xOffset, 0, zOffset))
 end
 
+function RoundManager:UpdatePickups(deltaTime)
+    if self.roundStart then
+        for _, padData in pairs(self.healthPads) do
+            padData.Cube.Orientation += Vector3.new(1, 1, 0)
+
+            if not padData.isActive then
+                if padData.currTime <= 0 then
+                    padData.isActive = true
+                    padData.UI.Enabled = false
+                    padData.currTime = padData.cooldown
+    
+                    padData.Cube.Transparency = 0
+                end
+    
+                padData.currTime -= deltaTime
+                padData.currTime = math.clamp(padData.currTime, 0, padData.cooldown)
+                local durationText = string.format("%0.2f", padData.currTime)
+                padData.Timer.Text = "["..durationText.."]"
+
+                continue
+            end
+
+            local playerList = {}
+            for _, plr in pairs(game.Players:GetChildren()) do
+                local char = plr.Character
+                table.insert(playerList, char)
+            end
+
+            local overlap = OverlapParams.new()
+            overlap.FilterDescendantsInstances = {playerList}
+            overlap.FilterType = Enum.RaycastFilterType.Include
+
+            local touchedObjects = workspace:GetPartsInPart(padData.hitbox, overlap)
+
+            for i=1, #touchedObjects do
+                local object = touchedObjects[i]
+                local parent = object.Parent
+
+                local humanoid = parent:FindFirstChild("Humanoid")
+                if not humanoid then
+                    continue
+                end
+
+                if humanoid.Health <= 0 then
+                    continue
+                end
+
+                if not padData.isActive then
+                    continue
+                end
+
+                padData.isActive = false
+                padData.Cube.Transparency = 1
+                padData.Cube.Idle.Health.Enabled = false
+                padData.UI.Enabled = true
+
+                padData.Cube.Burst.Health:Emit(8)
+                padData.Cube.Burst.Ring:Emit(3)
+
+                HealthManager:Heal(parent, 25)
+            end
+        end
+    end
+end
+
 function RoundManager:Update(deltaTime)
     self.currTick += deltaTime
+
+    self:UpdatePickups(deltaTime)
 
     if self.currTick >= self.maxTick then
         self.currTick = 0
@@ -154,6 +257,8 @@ function RoundManager:Update(deltaTime)
                     self.startCountDown = false
                     self.countDown = 0
 
+                    self:ConfigurePickups(self.Map.HealthPads)
+
                     Events.Server_Client.CountDown:FireAllClients("Round", self.roundDuration)
 
                     self:TeleportAllPlayers()
@@ -168,6 +273,8 @@ function RoundManager:Update(deltaTime)
             if self.roundDuration <= 0 then
                 self.roundStart = false
                 self.roundDuration = 0
+
+                self:CleanupPickups()
 
                 self:ResetAllPlayers()
 
